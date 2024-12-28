600531adf5b432b1f8fac9c62c777a5754d3626a
Jatinemmet <164749317+Jatinemmet@users.noreply.github.com>
2024-12-28 17:52:47 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function clearScreen() {
+    document.getElementById('screen').innerText = '';
+}
+
+function deleteLast() {
+    let screen = document.getElementById('screen');
+    screen.innerText = screen.innerText.slice(0, -1);
+}
+
+function appendToScreen(value) {
+    let screen = document.getElementById('screen');
+    screen.innerText += value;
+}
+
+function calculate() {
+    let screen = document.getElementById('screen');
+    try {
+        screen.innerText = eval(screen.innerText);
+    } catch {
+        screen.innerText = 'Error';
+    }
+}
